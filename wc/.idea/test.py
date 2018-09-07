ae94a60383e45c5cc92e11c13c800f2d81552633
侯国鑫 <1604992695@qq.com>
2018-09-07 19:28:45 +0800

    Signed-off-by: 侯国鑫 <1604992695@qq.com>

--- wc/.idea/test.py
+++ wc/.idea/test.py
@@ -41,36 +41,40 @@ def readfile(name):
                         flage = 0
 
     return  row,blank_row,code_row,comment_row,chars,word,chaniese
-
-if func!="-s" :
-    res = readfile(sys.argv[2])
-    if func == "-c" :
-        print("字符数为：" ,res[4])
-    if func == "-l" :
-        print("总行数为：" , res[0])
-    if func == "-w":
-        print("单词数为:" ,res[5],"\n中文字数为：",res[6])
-    if func == "-a" :
-        print("空行数为：" , res[1],"\n代码行为：" , res[2],"\n注释行为：",res[3])
-    if func == "all" :
-        print("总行数为：" , res[0])
-        print("空行数为：" , res[1])
-        print("代码行为：" , res[2])
-        print ("注释行为：",res[3])
-        print("字符数为：" ,res[4])
-        print("单词数为：" ,res[5])
-        print ("中文字数为：",res[6])
+	
+res = readfile(sys.argv[2])
+if func == "-c" :
+	print("字符数为：" ,res[4])
+if func == "-l" :
+    print("总行数为：" , res[0])
+if func == "-w":
+    print("单词数为:" ,res[5])
+    print ("中文字数为：",res[6])
+if func == "-a" :
+    print("空行数为：" , res[1])
+    print("代码行为：" , res[2])
+    print ("注释行为：",res[3])
+if func == "all" :
+    print("总行数为：" , res[0])
+    print("空行数为：" , res[1])
+    print("代码行为：" , res[2])
+    print ("注释行为：",res[3])
+    print("字符数为：" ,res[4])
+    print("单词数为:" ,res[5])
+    print ("中文字数为：",res[6])
 if func == "-s" :
     filelist = os.listdir(os.getcwd())
     for n in filelist :
         if os.path.isfile(n):
             if os.path.splitext(n)[1] == os.path.splitext(sys.argv[3])[1]:
-                res1 = readfile(n)
                 if func2 == "-c" :
-                    print("字符数为：" ,res1[4])
+                    print("字符数为：" ,res[4])
                 if func2 == "-l" :
-                    print("总行数为：" , res1[0])
+                    print("总行数为：" , res[0])
                 if func2 == "-w":
-                    print("单词数为:" ,res1[5],"\n中文字数为：")
+                    print("单词数为:" ,res[5])
+                    print ("中文字数为：",res[6])
                 if func2 == "-a" :
-                    print("空行数为：" , res1[1],"\n代码行为：" , res1[2],"\n注释行为：",res1[3])
+                    print("空行数为：" , res[1])
+                    print("代码行为：" , res[2])
+                    print ("注释行为：",res[3])
